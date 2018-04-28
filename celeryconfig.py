44873f889e8c9bb33897a31a4220437c9153ddb2
ajin94 <ajinsanut@gmail.com>
2018-04-28 15:23:49 +0530

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,6 +1,6 @@
-# specify whihc messaging brocker to be used
+
 broker_url = 'amqp://localhost//'
-# can specify the tasks that celery can to trigger
-imports = ('celery_learn.tasks', )
-# backend for celery
+
 result_backend = 'db+postgresql://postgres:useruser@localhost/celery_test_base'
+
+imports = ('celery_learn.tasks', )
