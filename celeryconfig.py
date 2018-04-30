a5b20d823653b6d1f414259e5c0d38b4d30055af
ajin94 <ajinsanut@gmail.com>
2018-04-30 08:51:13 +0530

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,4 @@
 
 broker_url = 'amqp://localhost//'
 
-imports = ('celery_learn.tasks')
+imports = ('celery_learn')
--- tasks.py
+++ tasks.py
@@ -1,27 +1,10 @@
-from celery_utils import app
+from celery import app
 import time
-import os
-import django
-import sys
-from django.core.wsgi import get_wsgi_application
-
-sys.path.append("/home/mis/work/attorney-bar/webapp/")
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
-django.setup()
-get_wsgi_application()
-
-from attorney_bar_app.models import CeleryTest
 
 @app.task
 def addition(s,w):
     time.sleep(20)
     return s+w
-
-@app.task
-def insertion(name, email):
-    time.sleep(2)
-    CeleryTest.objects.create(name=name, email=email)
-    return "worked"
 
 @app.task
 def get_no_char(string):
