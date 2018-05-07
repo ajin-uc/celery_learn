5f1a720ca0231d5d678249c41fc3498f7e078d37
ajin94 <ajinsanut@gmail.com>
2018-05-07 12:55:53 +0530

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -6,11 +6,14 @@ imports = ('tasks',)
 
 task_default_queue = 'default-queue'
 task_default_exchange_type = 'direct'
+task_default_routing_key = 'default-queue'
 
 task_queues = (
     Queue('adition-queue', routing_key='adition-queue'),
     Queue('periodic-queue', routing_key='periodic-queue'),
     Queue('testing-queue', routing_key='testing-queue'),
+    Queue('count-queue', routing_key='count-queue'),
+    Queue('reverse-queue', routing_key='reverse-queue'),
 )
 
 task_create_missing_queues = False
